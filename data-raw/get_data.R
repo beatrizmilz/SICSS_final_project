ec415b206a3dc8279f92e03cd1edf4cde36776bd
Beatriz Milz <beatriz.milz@hotmail.com>
2021-06-24 17:02:41 -0300

    avanca na coleta

--- data-raw/get_data.R
+++ data-raw/get_data.R
@@ -18,7 +18,8 @@ paginas_mudancas_climaticas <- c(
   tibble::enframe() %>%
   dplyr::mutate(value = stringr::str_squish(value)) %>%
   dplyr::distinct(value) %>%
-  dplyr::filter(value != "Predefinição:Mudança do clima") %>%
+  dplyr::filter(value != "Predefinição:Mudança do clima",
+                value != "Predefinição Discussão:Mudança do clima (página não existe)") %>%
   dplyr::rename("page_name" = value)
 
 
@@ -28,20 +29,41 @@ dplyr::glimpse(paginas_mudancas_climaticas)
 
 usethis::use_data(paginas_mudancas_climaticas, overwrite = TRUE)
 
+
+
 # vamos buscar então as categorias dessas páginas --------
 
+modified_get_categories <- function(page) {
+  print(page)
+  wikihistory::get_categories(page, lang = "pt")
+}
+
 categorias <- purrr::map_dfr(.x = paginas_mudancas_climaticas$page_name,
-               .f = wikihistory::get_categories,
-               lang = "pt")
+             #  .f = wikihistory::get_categories,
+             modified_get_categories
+               #lang = "pt"
+             )
+
 
 usethis::use_data(categorias, overwrite = TRUE)
 
 View(categorias)
 
+# ATÉ AQUI TÁ OK!
+
+
 # qual é o historico de mudanças dessas páginas?
 
+possibly_get_history <- function(page){
+  history <- wikihistory::get_history(page)
+  print(page)
+  history
+}
+
+possibly_get_history(paginas_mudancas_climaticas$page_name[1])
+
 historico_mudancas_raw <- purrr::map_dfr(.x = paginas_mudancas_climaticas$page_name,
-               .f = wikihistory::get_history,
+               .f = purrr::possibly(wikihistory::get_history, otherwise = "Erro"),
                lang = "pt")
 
 historico_mudancas <-  wikihistory::clean_history(historico_mudancas_raw, lang = "pt")
